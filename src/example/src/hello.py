cc565b7994426229147d98ce08d5f9a393bda72b
lee-910530 <jacky0970711835@gmail.com>
2021-10-22 16:55:59 +0800

    a a

--- src/example/src/hello.py
+++ src/example/src/hello.py
@@ -1,4 +1,14 @@
 #! /usr/bin/env python
 import rospy                                
 rospy.init_node("hello_python_node")     
-rospy.loginfo("Hello World")                
+from time import sleep
+n=1
+while n<11:
+    if n==10:
+	sleep(1)
+	print("time over")
+	n+=1
+    elif n<10:
+	sleep(1)
+	print(n)
+	n+=1   
