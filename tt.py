ea7e4ac0b33f88cc1241da50cf619e90d8ca6fd7
lee-910530 <jacky0970711835@gmail.com>
2022-09-26 17:47:19 +0800

    difbqwiuervn

--- tt.py
+++ tt.py
@@ -1,4 +1,6 @@
 from collections import Counter
+import numpy as np
+import math
 
 
 class Solution(object):
@@ -95,11 +97,58 @@ class Solution(object):
         new = " ".join((new))
         return (new)
 
+    def rotate(self, matrix):
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for i in range(n):
+            matrix[i].reverse()
+        return matrix
+
+    def spiralOrder(self, matrix):
+        return matrix and [*matrix.pop(0)] + self.spiralOrder([*zip(*matrix)][::-1])
+
+    def minDistance(self, word1, word2) :
+        list1 = list(word1)
+        list2 = list(word2)
+        return(list(set(list1).intersection(set(list2))))
+
+    def permute(self, nums):
+        pass
 # ...........................................................
 ss = Solution()
 
 # .........................data..............................
-s = "foo", t = "bar"
+s = "a1b2"
 # .........................main..............................
 if __name__ == '__main__':
-    len(set(s)) == len(set(t)) == len(set(zip(s, t)))
+    # i = m
+    # count = 0
+    # ans = math.factorial(n)
+    # while i >= 10 :
+    #     i,x = divmod(m,10)
+    #     if x == 0:
+    #         count += 1
+    #         m = i
+    #     else :
+    #         break
+    # return (count)
+    #........................................
+    list_s_min = list(s)
+    list_s_max = list(s.upper())
+    ans = []
+    x = zip(list_s_min,list_s_max)
+    print(list(x))
+    x= [num for i,x in (range(10))(range(10))]
+    print(x)
+
+
+
+
+
+
+
+
+
+
